b3a86173bc3846b2d52d560010b03f85cfa60480
cxie0001trevor <trevor8961@gmail.com>
2020-12-15 22:59:14 +0800

    Update install script: Change timezone; Change pipenv install arguments;

--- py3/custom/install.sh
+++ py3/custom/install.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
+echo "> setting timezone to Asia/Shanghai"
+sudo unlink /etc/localtime 
+sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 # pip config
 echo "> Initial pip configuration file ..."
 mkdir -p /home/jovyan/.config/pip
 cp pip.conf /home/jovyan/.config/pip/
 
 echo "> Install pipenv ..."
-sudo -H pip install pipenv > package_install.log
+pip install pipenv > package_install.log
 
 echo "> Creating Pipfile.lock"
-pipenv lock > package_install.log
+pipenv lock >> package_install.log
 
 echo "> Install required packages from Pipfile ..."
 # Ref: https://github.com/pypa/pipenv/issues/2183
 # > --deploy: current directory must have Pipfile.lock and it should be up-to-date.
 # > --system: Required Pipfile.lock presents.
-pipenv install --deploy --system > package_install.log
+pipenv install --system >> package_install.log
 
 echo "> Setting up notebook extensions ..."
 # Install jupyter themes
-sudo -H pip install --no-cache-dir jupyterthemes > package_install.log
+pip install --no-cache-dir jupyterthemes >> package_install.log
 
 # Install jupyter extensions
-sudo -H pip install --no-cache-dir jupyter_contrib_nbextensions > package_install.log
+pip install --no-cache-dir jupyter_contrib_nbextensions >> package_install.log
 
 # It copies the nbextensions' javascript and css files 
 # into the jupyter server's search directory, and edits
