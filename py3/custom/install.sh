cd39fa3d41e54d8367c0b9378458b710fea11460
Chuangfu Xie <182264116@qq.com>
2020-12-22 17:43:25 +0800

    Update customise packages requirements

--- py3/custom/install.sh
+++ py3/custom/install.sh
@@ -7,21 +7,22 @@ cp pip.conf /home/jovyan/.config/pip/
 echo "> Install pipenv ..."
 sudo -H pip install pipenv > package_install.log
 
+rm Pipfile.lock
 echo "> Creating Pipfile.lock"
-pipenv lock > package_install.log
+pipenv lock >> package_install.log
 
 echo "> Install required packages from Pipfile ..."
 # Ref: https://github.com/pypa/pipenv/issues/2183
 # > --deploy: current directory must have Pipfile.lock and it should be up-to-date.
 # > --system: Required Pipfile.lock presents.
-pipenv install --deploy --system > package_install.log
+pipenv install --deploy --system > nbextensions_install.log
 
 echo "> Setting up notebook extensions ..."
 # Install jupyter themes
-sudo -H pip install --no-cache-dir jupyterthemes > package_install.log
+sudo -H pip install --no-cache-dir jupyterthemes >> nbextensions_install.log
 
 # Install jupyter extensions
-sudo -H pip install --no-cache-dir jupyter_contrib_nbextensions > package_install.log
+sudo -H pip install --no-cache-dir jupyter_contrib_nbextensions >> nbextensions_install.log
 
 # It copies the nbextensions' javascript and css files 
 # into the jupyter server's search directory, and edits
